6a452081201c25eb9b29018bad627b5c3a6c09bf
Zachary Stewart <zachary@zstewart.com>
2016-04-02 12:52:22 -0400

    added get/setcar/cdr functions to IConsType

--- kos-lisp/KOSLisp/Types/IConsType.cs
+++ kos-lisp/KOSLisp/Types/IConsType.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ZStewart.KOSLisp.Types.TypeCategories;
 
 namespace ZStewart.KOSLisp.Types {
   /// <summary>
@@ -37,11 +38,38 @@ namespace ZStewart.KOSLisp.Types {
       ICons.__class__ = TypeType.Type;
       ICons.__bases__ = Create(ObjectType.Object, NilType.Nil);
       ICons.__new__ = New;
+      ICons._list_methods = new ListMethods {
+        __getcar__ = GetCar,
+        __getcdr__ = GetCdr,
+      };
     }
 
     private static LispObject New (LispObject subtype, LispObject args) {
       // TODO(zstewar1)
       return null;
+    }
+
+    private static LispObject GetCar (LispObject instance) {
+      // TODO(zstewar1): Maybe check isinstance? Maybe not though, all subtypes should
+      // have initialized with our New, so instance must be a ConsType. If a subtype, then
+      // it would either have an __dict__ if a dynamic type, or be a real C# subclass if
+      // a static subtype. Either way, it should be safe to use a check/convert on the C#
+      // type.
+      if (instance is IConsType) {
+        return (instance as IConsType).Car;
+      } else {
+        // TODO(zstewar1): Set error.
+        return null;
+      }
+    }
+
+    private static LispObject GetCdr (LispObject instance) {
+      if (instance is ConsType) {
+        return (instance as ConsType).Cdr;
+      } else {
+        // TODO(zstewar1): Set Error.
+        return null;
+      }
     }
     #endregion Static Type Setup 
 
