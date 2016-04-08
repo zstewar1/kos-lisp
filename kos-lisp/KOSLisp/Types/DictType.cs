93e5f3598fc0f7598bb3154af6b7ed8636dda29a
Zachary Stewart <zachary@zstewart.com>
2016-04-07 21:18:51 -0400

    start adding a dict type

---
+++ kos-lisp/KOSLisp/Types/DictType.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZStewart.KOSLisp.Types {
+  public class DictType {
+    #region Static Type Setup
+    public static readonly LispTypeObject Dict = new LispTypeObject();
+
+    static DictType () {
+      Dict.__name__ = "dict";
+      Dict.__class__ = TypeType.Type;
+      Dict.__bases__ = IConsType.ToLispTuple(ObjectType.Object);
+    }
+    #endregion Static Type Setup
+  }
+}
