758e2656386a32242a5229c826173b991f9ef235
Zachary Stewart <zachary@zstewart.com>
2016-05-24 19:10:57 -0400

    Update function call generator to be able to generate keyword argument function calls

--- ZStewart.KOSLisp.Compile.Generators.CSharp/CSharpFuncCallGenerator.cs
+++ ZStewart.KOSLisp.Compile.Generators.CSharp/CSharpFuncCallGenerator.cs
@@ -18,9 +18,16 @@ namespace ZStewart.KOSLisp.Compile.Generators.CSharp {
     /// </summary>
     public CSharpGenerator Function { get; }
     /// <summary>
-    /// A list of generators which generate the arguments to the function being called.
+    /// A list of generators which generate the positional arguments to the function being
+    /// called.
     /// </summary>
-    public ImmutableList<CSharpGenerator> Arguments { get; }
+    public ImmutableList<CSharpGenerator> PositionalArguments { get; }
+
+    /// <summary>
+    /// A dictionary of symbols to generators which generate the keyword arguments to the
+    /// function being called.
+    /// </summary>
+    public ImmutableDictionary<SymbolType, CSharpGenerator> KeywordArguments { get; }
 
     /// <summary>
     /// Creates a function call generator for the given AST op.
@@ -34,8 +41,12 @@ namespace ZStewart.KOSLisp.Compile.Generators.CSharp {
     /// </param>
     internal CSharpFuncCallGenerator(AstFuncCall op, CSharpGeneratorFactory factory) {
       Function = factory.Create(op.Function);
-      Arguments = ImmutableList.CreateRange(
-        op.Arguments.Select(arg => factory.Create(arg)));
+      PositionalArguments = ImmutableList.CreateRange(
+        op.PositionalArguments.Select(arg => factory.Create(arg)));
+      KeywordArguments = ImmutableDictionary.CreateRange(
+        op.KeywordArguments.Select(
+          kvp => new KeyValuePair<SymbolType, CSharpGenerator>(
+            kvp.Key, factory.Create(kvp.Value))));
     }
 
     /// <summary>
@@ -43,17 +54,35 @@ namespace ZStewart.KOSLisp.Compile.Generators.CSharp {
     /// </summary>
     public Expression Emit() {
       var listAdd = typeof(List<LispObject>).GetMethod("Add");
+      var dictAdd = typeof(Dictionary<SymbolType, LispObject>).GetMethod("Add");
       var callMethod = typeof(CallableOperations).GetMethod(
-        "Call", new Type[] {typeof(LispObject), typeof(List<LispObject>)});
+        "Call", new Type[] {
+          typeof(LispObject),
+          typeof(List<LispObject>),
+          typeof(Dictionary<SymbolType, LispObject>)
+        });
 
-      var expressions = new List<Expression>(2);
+      var expressions = new List<Expression>(3);
       expressions.Add(Function.Emit());
-      if (Arguments.Count > 0) {
+      if (PositionalArguments.Count > 0) {
         expressions.Add(Expression.ListInit(
           Expression.New(typeof(List<LispObject>)),
-          Arguments.Select(arg => Expression.ElementInit(listAdd, arg.Emit()))));
+          PositionalArguments.Select(
+            arg => Expression.ElementInit(listAdd, arg.Emit()))));
       } else {
         expressions.Add(Expression.New(typeof(List<LispObject>)));
+      }
+
+      if (KeywordArguments.Count > 0) {
+        expressions.Add(Expression.ListInit(
+          Expression.New(typeof(Dictionary<SymbolType, LispObject>)),
+          KeywordArguments.Select(
+            kvp => Expression.ElementInit(
+              dictAdd,
+              Expression.Constant(kvp.Key, typeof(SymbolType)),
+              kvp.Value.Emit()))));
+      } else {
+        expressions.Add(Expression.New(typeof(Dictionary<SymbolType, LispObject>)));
       }
 
       return Expression.Call(callMethod, expressions);
