be68a865e54ca2202cb4e0bf185413cfdd4e77ba
Zachary Stewart <zachary@zstewart.com>
2016-05-24 08:58:00 -0400

    Add new expression generators for importing

--- ZStewart.KOSLisp.Compile.Generators.CSharp/CSharpImportGenerator.cs
+++ ZStewart.KOSLisp.Compile.Generators.CSharp/CSharpImportGenerator.cs
@@ -7,6 +7,7 @@ using System.Linq.Expressions;
 using ZStewart.KOSLisp.Compile.AST;
 using ZStewart.KOSLisp.Modules;
 using ZStewart.KOSLisp.Types;
+using ZStewart.KOSLisp.Types.Helpers;
 
 namespace ZStewart.KOSLisp.Compile.Generators.CSharp {
   /// <summary>
@@ -68,6 +69,7 @@ namespace ZStewart.KOSLisp.Compile.Generators.CSharp {
             kvp => new KeyValuePair<SymbolType, CSharpBindingGenerator>(
               kvp.Key, factory.Create(kvp.Value))));
       }
+      AllTo = op.AllTo;
       Importer = importer;
     }
 
@@ -75,11 +77,116 @@ namespace ZStewart.KOSLisp.Compile.Generators.CSharp {
     /// Produce an expression representing a module import.
     /// </summary>
     public Expression Emit() {
-      return Binding.EmitSet(
-        Expression.Call(
-          Expression.Constant(Importer),
-          Importer.GetType().GetMethod("Import", new Type[]{typeof(IEnumerable<string>)}),
-          Expression.Constant(ModuleIdentifier)));
+      var imported = Expression.Variable(typeof(ModuleType), "imported");
+      var expressions = new List<Expression>(5);
+      expressions.Add(
+        Expression.Assign(
+          imported,
+          Expression.Call(
+            Expression.Constant(Importer),
+            Importer.GetType()
+              .GetMethod("Import", new Type[]{typeof(IEnumerable<string>)}),
+            Expression.Constant(ModuleIdentifier))));
+
+      AddBind(imported, expressions);
+      AddFrom(imported, expressions);
+      AddAll(imported, expressions);
+
+      expressions.Add(imported);
+
+      return Expression.Block(
+        typeof(LispObject),
+        ImmutableList.Create(imported),
+        expressions);
+    }
+
+    /// <summary>
+    /// if Binding is non-null, appends an expression which binds the imported module to
+    /// the expression list, otherwsie leave the expression list unchanged.
+    /// </summary>
+    private void AddBind(Expression imported, List<Expression> expressions) {
+      if (Binding != null) {
+        expressions.Add(Binding.EmitSet(imported));
+      }
+    }
+
+    /// <summary>
+    /// if FromImport is non-null, appends an expression which binds all of the symbols in
+    /// FromImport to their respective bindings.
+    /// </summary>
+    private void AddFrom(Expression imported, List<Expression> expressions) {
+      if (FromImport != null) {
+        var fromBinds = new List<Expression>(FromImport.Count);
+        foreach(var kvp in FromImport) {
+          fromBinds.Add(
+            kvp.Value.EmitSet(
+              Expression.Call(
+                typeof(ModuleType), "GetGlobal", null,
+                imported, Expression.Constant(kvp.Key))));
+        }
+        expressions.Add(Expression.Block(fromBinds));
+      }
+    }
+
+    /// <summary>
+    /// If the module to import to is non-null, adds an expression which binds all of the
+    /// symbols in the imported module to globals in the target module.
+    /// </summary>
+    private void AddAll(Expression imported, List<Expression> expressions) {
+      if (AllTo != null) {
+        var @break = Expression.Label("all-import-break");
+        var enumerator = Expression.Variable(typeof(LispObject), "loop-enumerator");
+        var enumcar = Expression.Variable(typeof(LispObject), "loop-enumerator-current");
+
+        expressions.Add(
+          // Check that the new module's dict is present; if not, do nothing.
+          Expression.IfThen(
+            Expression.NotEqual(
+              Expression.Field(imported, "__dict__"),
+              Expression.Constant(null, typeof(LispObject))),
+            // Dict is present:
+            Expression.Block(
+              typeof(void),
+              ImmutableList.Create(enumerator),
+              // Call dict.iter on the module dictionary.
+              Expression.Assign(
+                enumerator,
+                Expression.Call(
+                  typeof(LispObject), "Call", null,
+                  Expression.Field(imported, "__dict__"),
+                  Expression.Constant(SymbolType.Create("iter")))),
+              // Loop while enumerator !ref= Nil
+              Expression.Loop(
+                Expression.IfThenElse(
+                  Expression.ReferenceEqual(
+                    enumerator,
+                    Expression.Constant(NilType.Nil)),
+                  // Reference equals nil; break.
+                  Expression.Break(@break),
+                  // Not nil yet:
+                  Expression.Block(
+                    typeof(void),
+                    ImmutableList.Create(enumcar),
+                    // Get the car of the expression. This is the pair (key . value)
+                    Expression.Assign(
+                      enumcar,
+                      Expression.Call(
+                        typeof(ListOperations), "GetCar", null, enumerator)),
+                    // On the All-To module, set globals for each key in the module's
+                    // dictionary to the matched value.
+                    Expression.Call(
+                      typeof(ModuleType), "SetGlobal", null,
+                      Expression.Constant(AllTo),
+                      Expression.Call(
+                        typeof(ListOperations), "GetCar", null, enumcar),
+                      Expression.Call(
+                        typeof(ListOperations), "GetCdr", null, enumcar)),
+                    Expression.Assign(
+                      enumerator,
+                      Expression.Call(
+                        typeof(ListOperations), "GetCdr", null, enumerator)))),
+                @break))));
+      }
     }
   }
 }
